81136be94cd72484a20d8a49c8ee1817574ce8c4
Jeff-czh <chenzonghua2016@gmail.com>
2021-11-19 17:02:38 +0900

    テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true 
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
-
+  validates :nickname,               presence: true
+  validates :family_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "should be full-width Kanji" }
+  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "should be full-width Kanji" }
+  validates :family_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "should be full-width katakana" }
+  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "should be full-width katakana" }
+  validates :birthday,               presence: true
+  validates :password,               format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "should have both letters and numbers"}
 end
