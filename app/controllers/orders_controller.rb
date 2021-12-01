a7b48d461a11c891c5ecf6302c2305d4d6e9ed69
Jeff-czh <chenzonghua2016@gmail.com>
2021-12-01 11:15:45 +0900

    購入フォームの情報を保存する設定（create）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,16 +4,15 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
   end
-
-  # def new 
-  #   @item = Item.find(params[:id])
-  # end
-
   
-
-  # def create
-  #   @order_address = OrderAddress.new(order_params)
-  # end
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address .save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
 
   private
 
@@ -21,9 +20,9 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end 
 
-  # def order_params
-  #   params.require(:order_address).permit(:post_code, :prefecture_id, :city, :street_name, :bldg_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
-  # end
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :street_name, :bldg_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
 end
 
 
